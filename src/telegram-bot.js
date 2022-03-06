42dcdf3e34cb4cd1f89bc14d14351b4498b06da5
Guy Shuster <guyshu@post.bgu.ac.il>
2022-03-06 22:37:58 +0200

    add name differenciation in telegram channel

--- src/telegram-bot.js
+++ src/telegram-bot.js
@@ -10,14 +10,14 @@ class TelegramBotWrapper {
 
 	init() {
 		this.telegramBot.on('message', (message) => {
-			const { from: { is_bot }, chat: { id: chatId }, text } = message;
+			const { from: { is_bot }, chat: { id: chatId, first_name: firstName }, text } = message;
 
 			if (is_bot) {
 				return;
 			}
 
 			if (text === '/start') {
-				this.telegramBot.sendMessage(chatId, 'Hey Baybz ❤\nOCD bot updates here, stay tuned...');
+				this.telegramBot.sendMessage(chatId, `Hey ${firstName.toLowerCase() === 'may' ? 'Baybz ❤' : firstName}\nOCD bot updates here, stay tuned...`);
 				this.chatIds.push(chatId);
 			} else if (!this.sentWarning) {
 				this.telegramBot.sendMessage(chatId, 'All following messages here will be ignored, this channel is for updates only...');
