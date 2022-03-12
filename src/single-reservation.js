cf919565167a380fa52800a27075660cee4ca23f
Guy Shuster <guygosha@gmail.com>
2022-03-12 18:38:43 +0200

    finish single order structure

--- src/single-reservation.js
+++ src/single-reservation.js
@@ -13,7 +13,7 @@ export class ReservationData {
 	}
 }
 
-async function getAvailableTimeOnDate(requestedDate, requestedTime, amountOfPeople) {
+async function getAvailableTimeOnDate(requestedDate, requestedTime, amountOfPeople, timeout) {
 	const requestData = {
 		page_id: config.order.pageId,
 		locale: config.order.locale,
@@ -25,27 +25,23 @@ async function getAvailableTimeOnDate(requestedDate, requestedTime, amountOfPeop
 	};
 
 	try {
-		const { data: responseData } = await axios.post('https://ontopo.co.il/api/availability/searchAvailability', requestData, { headers: config.headers });
+		const { data: responseData } = await axios.post('https://ontopo.co.il/api/availability/searchAvailability', requestData, { headers: config.headers, timeout });
 
 		if (!responseData.availability_id) {
 			throw new Error('Get available time api error: wrong response format from server (no availability_id)');
 		} else if (!responseData.areas) {
-			return {};
+			throw new Error('Get available time error: no available time was found');
 		} else if (responseData.areas.length && responseData.areas[0].id && responseData.areas[0].options.length && responseData.areas[0].options[0].time) {
 			return { time: responseData.areas[0].options[0].time, availability_id: responseData.availability_id, area: responseData.areas[0].id };
 		} else {
-			throw new Error('Get available time api error: responseData.areas has a wrong format');
+			throw new Error('Get available time api error: responseData.areas had an unexpected format');
 		}
 	} catch (error) {
-		throw new Error(`Get available time native axios error: ${error.message}`);
+		throw new Error(`Get available time axios error: ${error.message}`);
 	}
 }
 
-async function chooseAvailableTimeOnDate(chosenDate, chosenTime, amountOfPeople, additionalAvailabilityData) {
-	if (!chosenTime) {
-		throw new Error('Choose available time error: no available time was found');
-	}
-
+async function chooseAvailableTimeOnDate(chosenDate, chosenTime, amountOfPeople, additionalAvailabilityData, timeout) {
 	const requestData = {
 		page_id: config.order.pageId,
 		locale: config.order.locale,
@@ -58,7 +54,7 @@ async function chooseAvailableTimeOnDate(chosenDate, chosenTime, amountOfPeople,
 	};
 
 	try {
-		const { data: responseData } = await axios.post('https://ontopo.co.il/api/availability/searchAvailability', requestData, { headers: config.headers });
+		const { data: responseData } = await axios.post('https://ontopo.co.il/api/availability/searchAvailability', requestData, { headers: config.headers, timeout });
 
 		if (!responseData.checkout_id) {
 			throw new Error('Choose available time error: no checkout_id was given from the server');
@@ -66,11 +62,11 @@ async function chooseAvailableTimeOnDate(chosenDate, chosenTime, amountOfPeople,
 
 		return responseData.checkout_id;
 	} catch (error) {
-		throw new Error(`Choose available time error: ${error.message}`);
+		throw new Error(`Choose available time axios error: ${error.message}`);
 	}
 }
 
-async function fillContactDetails(checkoutId, firstName, lastName, email, phone) {
+async function fillContactDetails(checkoutId, firstName, lastName, email, phone, timeout) {
 	const requestData = {
 		checkout_id: checkoutId,
 		region_code: config.order.regionCode,
@@ -82,56 +78,50 @@ async function fillContactDetails(checkoutId, firstName, lastName, email, phone)
 	};
 
 	try {
-		await axios.post('https://ontopo.co.il/api/checkout/checkoutContact', requestData, { headers: config.headers });
+		await axios.post('https://ontopo.co.il/api/checkout/checkoutContact', requestData, { headers: config.headers, timeout });
 	} catch (error) {
-		throw new Error(`Fill contact details error: ${error.message}`);
+		throw new Error(`Fill contact details axios error: ${error.message}`);
 	}
 }
 
-async function completeCheckout(checkoutId, phone) {
+async function completeCheckout(checkoutId, phone, timeout) {
 	const requestData = {
 		checkout_id: checkoutId,
 		phone,
 	};
 
 	try {
-		const { data: responseData } = await axios.post('https://ontopo.co.il/api/checkout/checkoutComplete', requestData, { headers: config.headers });
+		const { data: responseData } = await axios.post('https://ontopo.co.il/api/checkout/checkoutComplete', requestData, { headers: config.headers, timeout });
 
 		if (responseData.data && responseData.data.ticketUrl) {
 			return responseData.data.ticketUrl;
+		} else {
+			throw new Error('Checkout completion error: no reservation url was returned');
 		}
 	} catch (error) {
-		throw new Error(`Checkout completion error: ${error.message}`);
+		throw new Error(`Checkout completion axios error: ${error.message}`);
 	}
 }
 
-export async function makeReservation(reservationData, { testing = false, logger = { log: console.log } } = {}) {
+export async function makeReservation(reservationData, { testing = false, requestTimeout = 0 } = {}) {
 	if (!(reservationData instanceof ReservationData)) {
 		throw new Error('The function must receive an reservation data instance');
 	}
 
-	try {
-		const { time: chosenTime, ...additionalAvailabilityData } = await getAvailableTimeOnDate(reservationData.date, reservationData.time, reservationData.amountOfPeople);
-		const checkoutId = await chooseAvailableTimeOnDate(reservationData.date, chosenTime, reservationData.amountOfPeople, additionalAvailabilityData);
-		await fillContactDetails(checkoutId, reservationData.firstName, reservationData.lastName, reservationData.email, reservationData.phone);
+	const { time: chosenTime, ...additionalAvailabilityData } = await getAvailableTimeOnDate(reservationData.date, reservationData.time, reservationData.amountOfPeople, requestTimeout);
+	const checkoutId = await chooseAvailableTimeOnDate(reservationData.date, chosenTime, reservationData.amountOfPeople, additionalAvailabilityData, requestTimeout);
+	await fillContactDetails(checkoutId, reservationData.firstName, reservationData.lastName, reservationData.email, reservationData.phone, requestTimeout);
 
-		if (testing) {
-			logger.log(`Success! got to one step before reservation completion. Checkout ID: ${checkoutId}`);
-		} else {
-			const ticketUrl = await completeCheckout(checkoutId, '0549439700');
-	
-			if (ticketUrl) {
-				// TODO: Succeeded
-			} else {
-				// TODO: Failed
-			}
-		}
-	} catch (error) {
-		logger.log(error.message);
+	if (testing) {
+		console.log(`Success! got to one step before reservation completion. Checkout ID: ${checkoutId}`);
+		return;
 	}
+	
+	const reservationUrl = await completeCheckout(checkoutId, reservationData.phone);
+	return reservationUrl;
 }
 
 // TODO: dont forget not to have for await on axios request to searchAvailability.
 // Launch them all, the one that gets back first wins
 // TODO: add telegram logs
-// TODO: maybe add request timeouts
+// TODO: maybe add request timeouts, increasing time as we go on
