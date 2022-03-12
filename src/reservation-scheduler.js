0df6693ae4ddfd8490453b6bddd3d948a79efb4e
Guy Shuster <guygosha@gmail.com>
2022-03-12 19:31:23 +0200

    more plans

--- src/reservation-scheduler.js
+++ src/reservation-scheduler.js
@@ -9,7 +9,7 @@
 
 // Scheduling:
 // Use date-fns
-// Make date priorities for each order
+// Make date priorities for each order, and add specific times to these dates because ontopo only answers to times when the restaurant is working
 // Check date validity with intervals 
 // If any of the config values don't pass the validity, throw error and dont launch bot at all.
 // When the bot is "released" and running on it's own we need everything to be perfect and automatic, so checking stuff in config before the cron job is launched is key!
